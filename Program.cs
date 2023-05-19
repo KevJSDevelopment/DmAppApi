0afc1020498449cba535151896de9827f480d6b0
KevJSDev <KevJSDev@users.noreply.github.com>
2023-05-18 21:39:41 -0400

    separated discord bot and api

--- Program.cs
+++ Program.cs
@@ -5,45 +5,10 @@ namespace DMApp
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            await host.StartAsync();
-            await new Program().MainAsync();
-            await host.StopAsync();
+            CreateHostBuilder(args).Build().Run();
         }
-        
-        private DiscordSocketClient? _client;
-
-        public async Task MainAsync()
-        {
-            _client = new DiscordSocketClient();
-
-            _client.Log += Log;
-
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-
-            var token = Environment.GetEnvironmentVariable("Discord_Client_Token");
-
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
-
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
-
-            // Block this task until the program is closed.
-            await Task.Delay(-1);
-        }
-
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
