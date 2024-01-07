072aa795577efaa6506796e59050ac3226a5c30a
KevJSDevelopment <Kshhn12@gmail.com>
2024-01-07 11:42:22 -0500

    added summary for session

--- Models/Session.cs
+++ Models/Session.cs
@@ -14,6 +14,8 @@ namespace DMApp.Models
         public string? Name { get; set; }
         public int? SessionNumber { get; set; }
 
+        public string? Summary { get; set; }
+
         public int? CampaignId { get; set; }
         public Campaign? Campaign { get; set; }
 
